8068b56f936155238ae4e4611ebbafba6aad7f4f
Paul Bernard-Hall <p.bernardhall@gmail.com>
2020-11-19 16:12:39 -0500

    Added #container & minor styling

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,17 +1,16 @@
-// Photos.js
 import React from "react";
 import { useFetch } from "./hooks";
-
 import { useFetch2 } from "./hooks2";
+import "../App.css";
 
 function Pokemon() {
-	let rand = Math.ceil(Math.random() * 150);
-	const [data, loading] = useFetch(rand);
-	const [data2, loading2] = useFetch2(rand);
+	let rand = Math.ceil(Math.random() * 400);
+	const [data, loadingImg] = useFetch(rand);
+	const [data2, loadingFlav] = useFetch2(rand);
 
 	return (
-		<>
-			{loading ? (
+		<div id="container">
+			{loadingImg ? (
 				<>
 					"Loading image..." <br />
 				</>
@@ -21,8 +20,7 @@ function Pokemon() {
 					<img
 						src={
 							data.sprites.other["official-artwork"].front_default
-								? data.sprites.other["official-artwork"]
-										.front_default
+								? data.sprites.other["official-artwork"].front_default
 								: data.sprites.front_default
 						}
 						alt={""}
@@ -32,44 +30,12 @@ function Pokemon() {
 					/>
 				</>
 			)}
-			{loading2 ? (
+			{loadingFlav ? (
 				"Loading flavor text..."
 			) : (
-				// <>
-				// 	<h3>{data.name.toUpperCase()}</h3>
-				// 	<img
-				// 		src={
-				// 			data.sprites.other["official-artwork"].front_default
-				// 				? data.sprites.other["official-artwork"]
-				// 						.front_default
-				// 				: data.sprites.front_default
-				// 		}
-				// 		alt={""}
-				// 		style={{
-				// 			maxWidth: "100%",
-				// 		}}
-				// 	/>
-				// </>
-
-				<>
-					{/* <h3>{data2.names[8].name}</h3> */}
-					<p>
-						{
-							data2.flavor_text_entries[3].flavor_text
-
-							// data2.flavor_text_entries.forEach(function (
-							// 	value,
-							// 	index
-							// ) {
-							// 	if (value.language.name === "en") {
-							// 		arr.push[value.flavor_text];
-							// 	}
-							// })
-						}
-					</p>
-				</>
+				<p>{data2.flavor_text_entries[0].flavor_text}</p>
 			)}
-		</>
+		</div>
 	);
 }
 export default Pokemon;
